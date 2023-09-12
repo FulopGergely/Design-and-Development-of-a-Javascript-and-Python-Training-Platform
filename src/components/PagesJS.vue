18250f6ad5542e2b7a3263fefc6946d06fbed9d5
FulopGergely <fgergo91@gmail.com>
2023-09-12 09:45:44 +0200

    created dynamic pages

---
+++ src/components/PagesJS.vue
@@ -0,0 +1,29 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "PagesJS",
+  props: ['value'],
+  data() {
+    return {
+      users: ['1', '2', '3'], // Példa felhasználók
+    };
+  },
+  mounted() {
+
+  },
+  watch: {
+  }
+})
+</script>
+
+<template>
+  <div>
+    <router-link v-for="user in users" :key="user" :to="'/javascript/' + user">{{ user }}</router-link>
+    <input
+    type="text"
+    :value="value"
+    @input="$emit('update:value', $event.target.value)"
+    />
+  </div>
+</template>
