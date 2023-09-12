9a9d75026f9baf1d7d343275656016e03adae0c4
FulopGergely <fgergo91@gmail.com>
2023-09-12 09:43:54 +0200

    install Vuex

--- src/main.js
+++ src/main.js
@@ -5,8 +5,25 @@ import { basicSetup } from 'codemirror'
 import VueCodemirror from 'vue-codemirror'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import { createStore } from 'vuex'
+
+// Vuex
+const index = createStore({
+    state () {
+        return {
+            count: 0
+        }
+    },
+    mutations: {
+        increment (state) {
+            state.count++
+        }
+    }
+})
 
 const app = createApp(App)
+
+app.use(index) //Vuex
 
 app.use(router)
 
--- src/views/JavascriptView.vue
+++ src/views/JavascriptView.vue
@@ -3,20 +3,26 @@ import {defineComponent} from 'vue'
 import FootButtons from "@/components/FootButtons.vue";
 import JavascriptCodeEditor from "@/components/JavascriptCodeEditor.vue";
 import config from "../assets/js_alapok.js";
+import PagesJS from "@/components/PagesJS.vue";
 config.tasks = config.tasks.map( (v,i) => v={ id:i+1, ...v } )
 
 export default defineComponent({
   name: "JavascriptView",
-  components: {FootButtons, JavascriptCodeEditor},
+  components: {PagesJS, FootButtons, JavascriptCodeEditor},
     data() {
       return {
         data: config,
         side: 0,
+        value: "asd"
       }
   },
   methods: {
     next() {
       this.side++;
+    },
+    increment() {
+      this.$store.commit('increment')
+      console.log(this.$store.state.count)
     },
     prev() {
       this.side--;
@@ -33,18 +39,29 @@ export default defineComponent({
         <div class="container-fluid py-5">
           <div>
             <div v-for="item in data.tasks" :key="item.id">
-              <div v-if="1===item.id">
+              <div v-if="side===item.id">
                 <h2>{{ item.q2 }}</h2>
                 <p>{{ item.question }}</p>
               </div>
             </div>
           </div>
+          <input>
         </div>
 
       </div>
+
+      {{value}}
+      <PagesJS/>
       <JavascriptCodeEditor/>
-      <FootButtons @prev="prev" @next="next"/>
+      <FootButtons
+          @prev="prev"
+          @next="next"
+      />
       <p>{{side}}</p>
+      <button type="button"
+              class="btn btn-success"
+              @click="increment"
+      >gomb++</button>
     </div>
 
 
